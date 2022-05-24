05fcdd7da7bb6a06fbe3192776d0e6958174718d
lwl <lwl@123.com>
2022-05-24 15:54:32 +0800

    Second Commit

--- src/main/java/SEU/LWL/Git/GitTest.java
+++ src/main/java/SEU/LWL/Git/GitTest.java
@@ -6,5 +6,7 @@ package SEU.LWL.Git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("First Git Test!");
+        //第二版新增的一行！
+        System.out.println("Second Git Test!");
     }
 }
