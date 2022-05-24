727ca2a871c3327cfa96cf3c413c1480564ae508
lwl <lwl@123.com>
2022-05-24 15:54:05 +0800

    First Commit

---
+++ src/main/java/SEU/LWL/Git/GitTest.java
@@ -0,0 +1,10 @@
+package SEU.LWL.Git;
+
+/**
+ * 2022-05-24 15:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("First Git Test!");
+    }
+}
