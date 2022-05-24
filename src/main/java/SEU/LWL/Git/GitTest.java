ce5754d3627d3d95d9f13fdf50e03cfde451ed50
lwl <lwl@123.com>
2022-05-24 15:54:54 +0800

    Third Commit

--- src/main/java/SEU/LWL/Git/GitTest.java
+++ src/main/java/SEU/LWL/Git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("First Git Test!");
         //第二版新增的一行！
         System.out.println("Second Git Test!");
+        //第三版新增的一行！
+        System.out.println("Third Git Test!");
     }
 }
